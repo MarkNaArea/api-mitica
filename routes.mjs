748b2307e77d2fd90243a9e728ca235ccc9fea3b
MarkNaArea <127256808+MarkNaArea@users.noreply.github.com>
2024-01-26 18:05:43 -0300

    skills

--- routes.mjs
+++ routes.mjs
@@ -6,6 +6,7 @@ import users from "./src/routes/authRoutes.mjs";
 import campanhas from "./src/routes/campanhaRoutes.mjs"
 import characters from "./src/routes/characterRoutes.mjs"
 import characterInvites from "./src/routes/characterRoutes.mjs"
+import skills from "./src/routes/skillRoutes.mjs"
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use("/auth", users);
 app.use("/campanhas", campanhas);
 app.use("/characters", characters);
 app.use("/characterinvites", characterInvites)
+app.use("/skills", skills)
 
 export default app
